d14088ae40e81e1768f8451dcb84750a2d759712
Shaily Tripathi <123561590+shaz12t@users.noreply.github.com>
2024-07-20 23:03:24 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class main{
+    public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    if(a==b){
+        System.out.println("yes the a and b are equal");
+    }
+    else if(a>b){
+        System.out.println("a is greater than b");
+    }
+    else{
+    
+            System.out.println("a is less than b");
+        }
+    }
+
+}
